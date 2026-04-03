60979e1ee0ae253f9b434bda2146cd9e305c22af
Ang Chen <chenang@outlook.com>
2026-04-03 15:07:35 +0800

    Refactor matrix visualization code and update comments for clarity

--- matrices_images_coordinates_matlab.m
+++ matrices_images_coordinates_matlab.m
@@ -2,26 +2,34 @@ clear;
 close all;
 clc;
 
+%% fprintf() is a function that allows you to print formatted data to the command window.
 a = 1;
 fprintf('%d\n',a);
 
-A = 1*[[1,2];
-     [3,4]];
+%% Different ways to display the matrix A as an image
+A = 1*[[1,145];
+     [212,355]];
 
-
+% 1. Using imshow(A) with default settings, displaying only [0,1] values
 figure;
 subplot(1,3,1);
 imshow(A);
+set(gca, 'YDir', 'normal')
 colormap("gray");
 colorbar;
 
+% 2. Using imshow(A,[]) to display the full range of values in A
 subplot(1,3,2);
 imshow(A, []);
+set(gca, 'YDir', 'normal')
 colormap("gray");
 colorbar;
 
+
+% 3. Using imagesc(A) to display the full range of values in A
 subplot(1,3,3);
 imagesc(A);
+set(gca, 'YDir', 'normal')
 axis equal tight;
 colormap("gray");
 colorbar;
